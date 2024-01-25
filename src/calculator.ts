b319eccae8cb143e2a7acb00c2e38cdb3694244d
yishan tsai <yishan13.tsai@gmail.com>
2024-01-25 22:19:29 +0800

    5/15

--- src/calculator.ts
+++ src/calculator.ts
@@ -5,6 +5,7 @@
  */
 export function add(a: number, b: number): number {
     // 在此實現函式
+    return a + b
 }
 
 /**
@@ -14,6 +15,7 @@ export function add(a: number, b: number): number {
  */
 export function subtract(a: number, b: number): number {
     // 在此實現函式
+    return a - b
 }
 
 /**
@@ -23,6 +25,7 @@ export function subtract(a: number, b: number): number {
  */
 export function multiply(a: number, b: number): number {
     // 在此實現函式
+    return a * b
 }
 
 /**
@@ -32,16 +35,20 @@ export function multiply(a: number, b: number): number {
  */
 export function divide(a: number, b: number): number {
     // 在此實現函式
+    if (b === 0) {
+        throw new Error('Cannot divide by zero');
+    }
+    return a / b
 }
 
 /**
  * 實現加法函數，結果不能超過 100。
- * 
+ *
  * @param a 第一個加數。
  * @param b 第二個加數。
  * @returns 兩個數字的和，但不超過 100。
  */
 export function add100(a: number, b: number): number {
     const sum = a + b;
-    return sum > 100 ? 100 : sum;
-  }
+    return sum >= 100 ? 100 : sum;
+}
